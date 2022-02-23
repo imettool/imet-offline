b86f4950c971bfe366f8675fa58204541fa197f6
panos <panos.polis@gmail.com>
2022-02-23 19:12:51 +0200

    - db config fix

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'imet_db'),
+    'default' => env('DB_CONNECTION', 'ofac'),
 
     /*
     |--------------------------------------------------------------------------
