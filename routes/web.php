cc9e7461eae3f30ffc99220533de45980b8973ac
Andrea Marelli <andrea.marelli.dev@gmail.com>
2023-10-25 16:31:26 +0200

    Fixed issues with routes and SA1 save

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use AndreaMarelli\ImetCore\Controllers\Imet\Controller;
 use AndreaMarelli\ModularForms\Controllers\UploadFileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Redirect;
@@ -9,8 +10,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['web', 'auth'])->group(function () {
 
     Route::get('/', function () { return Redirect::to('confirm_user'); });
+    Route::view('/welcome', 'imet-core::welcome')->name(Controller::ROUTE_PREFIX.'welcome');
 
     // User routes
+    Route::get('users/{role_type?}', [UserController::class, 'index'])->name('imet-core::users');
+    Route::patch('users', [UserController::class, 'update_roles'])->name('imet-core::users_update');
     Route::get('confirm_user', [UserController::class, 'confirm_offline_user']);
     Route::patch('confirm_user', [UserController::class, 'update_offline_user'])->name('update_offline_user');
 
