e74a14f564c1f9285fa43dd137c1f65d34e92008
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-04-03 14:35:16 +0200

    Fixed issue with Biopama WMTS

--- resources/views/vendor/modular-forms/layouts/components/head.blade.php
+++ resources/views/vendor/modular-forms/layouts/components/head.blade.php
@@ -2,5 +2,5 @@
 
 {{-- hardcoded accessToken (not possible to push it into .env.imetoffline through updates --}}
 @include('imet-core::layouts.components.assets', [
-    'mapbox_token' => 'pk.eyJ1IjoiYmxpc2h0ZW4iLCJhIjoiMEZrNzFqRSJ9.0QBRA2HxTb8YHErUFRMPZg'
+    'mapbox_token' => 'pk.eyJ1IjoiamFtZXNkYXZ5IiwiYSI6ImNpenRuMmZ6OTAxMngzM25wNG81b2MwMTUifQ.A2YdXu17spFF-gl6yvHXaw'
 ])
