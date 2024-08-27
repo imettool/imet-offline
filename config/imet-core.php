7eb4b910bb1753ffd2cf6d358f31bb62b94956ef
Andrea Marelli <andrea.marelli.dev@gmail.com>
2024-08-27 07:09:38 +0200

    Composer & npm update

--- config/imet-core.php
+++ config/imet-core.php
@@ -3,6 +3,9 @@ return [
 
     'offline_version' => env('IMET_OFFLINE_VERSION', 'DEV'),
 
+    // API tokens
+    'mapbox_token' => \Illuminate\Support\Env::getOrFail('MAPBOX_ACCESS_TOKEN'),
+
     // Routes' prefixes
     'web_routes_prefix' => null,
     'api_routes_prefix' => null,
