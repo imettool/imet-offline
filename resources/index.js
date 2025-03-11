534d631b99dd95a733ac8979fae62baaea65e27a
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-03-11 08:04:56 +0100

    Upgrading to tailwindcss 4

--- resources/index.js
+++ resources/index.js
@@ -1,11 +1,9 @@
+// ##### Import CSS #####
 import './index.css';
 
 /* Import packages */
 import '@modular-forms/index.js';
-// import '@imet-core/index.js';
-
-// ##### Import local styles #####
-// import './index.css';
+import '@imet-core/index.js';
 
 // Global variables - make them accessible from modules and from blade views
 window.Laravel = window.Laravel || {};
@@ -13,8 +11,8 @@ window.Laravel = window.Laravel || {};
 // Apps
 window.OfflineImet = {}
 
-// import SettingsApp from "./js/SettingsApp.js";
-// window.OfflineImet.SettingsApp = SettingsApp;
-//
-// import PaUpdate from "./js/PaUpdate.js";
-// window.OfflineImet.PaUpdate = PaUpdate;
+import SettingsApp from "./js/SettingsApp.js";
+window.OfflineImet.SettingsApp = SettingsApp;
+
+import PaUpdate from "./js/PaUpdate.js";
+window.OfflineImet.PaUpdate = PaUpdate;
