d4c087b54e9751cdc4a5bd56c2bec5e0193a5aa7
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-05-02 17:51:48 +0200

    Added link to logs

--- resources/views/offline/footer.blade.php
+++ resources/views/offline/footer.blade.php
@@ -3,12 +3,19 @@ use App\Helpers\SoftwareUpdater;
 ?>
 <section id="imet_footer">
 
+    <!-- Logs -->
+    @if(Route::current()->getName() !== 'confirm_user')
+        <div>
+            <a href="/logs" class="!text-gray-600">
+                {!! \ModularForms\Helpers\Template::icon('rectangle-list') !!}
+            </a>
+        </div>
+    @endif
+
     <!-- Version -->
     <div>
         @lang('offline.version'): <span class="font-bold text-primary-600">{{ config('nativephp.version')  }}</span>
     </div>
-
-    <div></div>
 
     <!-- Copyright -->
     <div class="font-bold">{{ config('nativephp.copyright') }}</div>
