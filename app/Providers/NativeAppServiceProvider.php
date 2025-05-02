1b0ecbeede31c879145d5b5829817ec0d5c0a175
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-05-02 11:04:17 +0200

    Fixed DB structure

--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -13,7 +13,11 @@ class NativeAppServiceProvider implements ProvidesPhpIni
      */
     public function boot(): void
     {
-        Window::open();
+        Window::open()
+            ->width(1200)
+            ->height(800)
+            ->resizable()
+            ->title('IMET Offline Tool');
     }
 
     /**
@@ -22,6 +26,8 @@ class NativeAppServiceProvider implements ProvidesPhpIni
     public function phpIni(): array
     {
         return [
+            'upload_max_filesize' => '2048M',
+            'post_max_size' => '2048M',
         ];
     }
 }
