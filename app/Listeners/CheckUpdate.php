08d585b88b251f5dcff2484e8f832e7d21f1308b
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-05-30 17:35:40 +0200

    Test with updater listener

---
+++ app/Listeners/CheckUpdate.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use Log;
+use Native\Laravel\Events\AutoUpdater\CheckingForUpdate;
+use Native\Laravel\Events\AutoUpdater\DownloadProgress;
+use Native\Laravel\Events\AutoUpdater\Error;
+use Native\Laravel\Events\AutoUpdater\UpdateAvailable;
+use Native\Laravel\Events\AutoUpdater\UpdateDownloaded;
+use Native\Laravel\Events\AutoUpdater\UpdateNotAvailable;
+
+class CheckUpdate
+{
+    public function handle(CheckingForUpdate|UpdateAvailable|UpdateNotAvailable|DownloadProgress|UpdateDownloaded|Error $event): void
+    {
+        if($event instanceof CheckingForUpdate){
+            Log::info('Checking for updates...');
+        } elseif($event instanceof UpdateAvailable) {
+            Log::info('Update available: ' . $event->releaseName . ' (Version: ' . $event->version . ')');
+        } elseif($event instanceof UpdateNotAvailable) {
+            Log::info('No updates available.');
+        } elseif($event instanceof DownloadProgress) {
+            Log::info('Download progress: ' . $event->percent . '% (' . $event->transferred . ' of ' . $event->total . ' bytes)');
+        } elseif($event instanceof UpdateDownloaded) {
+            Log::info('Update downloaded successfully.');
+        } elseif($event instanceof Error) {
+            Log::error('Error occurred while checking for updates: ' . $event->message);
+        }
+    }
+
+}
