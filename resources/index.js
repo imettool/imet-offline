8b46e230c6364dda877ed96aa87dfe4b3d34008c
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-06-12 15:50:30 +0200

    Tests in listen NativePHP events

--- resources/index.js
+++ resources/index.js
@@ -7,6 +7,7 @@ import '@imet-core/index.js';
 
 // Global variables - make them accessible from modules and from blade views
 window.Laravel = window.Laravel || {};
+window.Native = window.Native || {};
 
 // Apps
 window.OfflineImet = {}
@@ -16,3 +17,26 @@ window.OfflineImet.SettingsApp = SettingsApp;
 
 import PaUpdate from "./js/PaUpdate.js";
 window.OfflineImet.PaUpdate = PaUpdate;
+
+//  #########  test in listen NativePHP events  #########
+window.Native.on("Native\\Laravel\\Events\\Windows\\WindowShown", (payload, event) => {
+    console.log('1. WindowShown event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\CheckingForUpdate", (payload, event) => {
+    console.log('2. CheckingForUpdate event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\Error", (payload, event) => {
+    console.log('3. Error event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\DownloadProgress", (payload, event) => {
+    console.log('4. UpdateAvailable event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\UpdateAvailable", (payload, event) => {
+    console.log('5. UpdateAvailable event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\UpdateDownloaded", (payload, event) => {
+    console.log('6. UpdateDownloaded event received:', payload, event);
+});
+window.Native.on("Native\\Laravel\\Events\\AutoUpdater\\UpdateNotAvailable", (payload, event) => {
+    console.log('7. UpdateNotAvailable event received:', payload, event);
+});
