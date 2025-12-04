f9b64229010dc7a963a14cfe48406c9b025bc120
Andrea Marelli <andrea.marelli.dev@gmail.com>
2025-12-04 14:09:15 +0100

    Missing upgrade in oecm\Modules\Evaluation\Objectives

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware(['web', 'auth'])->group(function () {
 
     Route::get('/', function () { return Redirect::to('confirm_user'); });
