6091a2bb2b67394353feece5f5a8aac6f5d09c8f
Andrea Marelli <andrea.marelli.dev@gmail.com>
2026-08-17 09:25:08 +0200

    [Feat] upgrade to imet-core 3.8.1

--- config/imet-core.php
+++ config/imet-core.php
@@ -9,4 +9,11 @@ return [
     'web_routes_prefix' => null,
     'api_routes_prefix' => null,
 
+    // CSV sample files: populate protected areas and species tables with data from CSVs if provided
+    'csv_sample_files' => [
+        'protected_areas' => env('CSV_PROTECTED_AREAS_SAMPLE_FILE', null),
+        'species' => env('CSV_SPECIES_SAMPLE_FILE', null),
+        'vernacular_names' => env('CSV_VERNACULAR_NAMES_SAMPLE_FILE', null),
+    ]
+
 ];
